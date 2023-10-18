b727466d53934c3115f4a1a99f0ecae49f36464b
s-prechtl <stefan@tague.at>
2023-10-18 09:31:58 +0200

    feat: compose-test

--- src/test/java/org/example/UserTest.java
+++ src/test/java/org/example/UserTest.java
@@ -22,8 +22,32 @@ public class UserTest {
     @BeforeAll
     public void setUp() throws SQLException {
         connection = DbInstance.getInstance(true);
-        purgeAllData();
         Statement statement = connection.createStatement();
+
+        statement.execute("""
+                create table user
+                (
+                    user_id int auto_increment primary key,
+                    email varchar(255) not null,
+                    password varchar(255) not null,
+                    first_name varchar(255) not null,
+                    last_name varchar(255) not null
+                );
+                """);
+
+        statement.execute("""
+                create table todo
+                (
+                    todo_id int auto_increment primary key,
+                    user_id int not null,
+                    description text not null,
+                    done tinyint(1) default 0 null,
+                    constraint todo_user_user_id_fk
+                    foreign key (user_id) references user (user_id)
+                );
+                                """);
+
+        purgeAllData();
         statement.execute(
                 "INSERT INTO user (user_id, email, password, first_name, last_name) VALUES (1, 'sprechtl@htl-steyr.ac.at', 'beidl', 'Stefan', 'Prechtl')");
         statement.execute(
